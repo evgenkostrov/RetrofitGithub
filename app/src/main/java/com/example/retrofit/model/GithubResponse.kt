cbbe42ac9d3ed6270a1c86df51a805423e76828b
evgenkostrov <evgenkostrov@gmail.com>
2022-03-02 23:31:21 +0300

    appliance

--- app/src/main/java/com/example/retrofit/model/GithubResponse.kt
+++ app/src/main/java/com/example/retrofit/model/GithubResponse.kt
@@ -1,7 +1,6 @@
 package com.example.retrofit.model
 
-data class GithubResponse (
-    val name:String,
-    val created_at:String
-
-        )
+data class GithubResponse(
+    val name: String,
+    val created_at: String
+)
